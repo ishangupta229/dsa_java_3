c995233eadf59e5d431353abbf03dfc4b7f96517
Ishan Gupta <ishangupta12345@yahoo.com>
2024-04-05 12:42:34 +0530

    p

--- week 21- Heaps/heaps_intro.java
+++ week 21- Heaps/heaps_intro.java
@@ -1,32 +1,36 @@
 import java.util.*;
+
 public class heaps_intro {
     static class minHeap {
-        static ArrayList <Integer> Heap = new ArrayList<>();
+        static ArrayList<Integer> Heap = new ArrayList<>();
 
         public static void add(int num) {
             Heap.add(num);
-            int i = Heap.size()-1;
-            while (Heap.get(i)<Heap.get((i-1)/2)) {
+            int i = Heap.size() - 1;
+            while (i > 0 && Heap.get(i) < Heap.get((i - 1) / 2)) {
+                // Swap elements
                 int tmp = Heap.get(i);
-                Heap.set(i, Heap.get((i-1)/2) );
-                Heap.set((1+i)/2, tmp);
-                i = (i+1)/2;
+                Heap.set(i, Heap.get((i - 1) / 2));
+                Heap.set((i - 1) / 2, tmp);
+                i = (i - 1) / 2;
             }
         }
+
         public static void print() {
             System.out.print(Heap);
         }
     }
+
     public static void main(String[] args) {
         minHeap heap = new minHeap();
-        minHeap.add(2);   
-        minHeap.add(3);   
-        minHeap.add(4);   
-        minHeap.add(5);   
-        minHeap.add(10);   
-        minHeap.add(1);   
-        minHeap.add(0);  
-        
+        heap.add(2);
+        heap.add(3);
+        heap.add(4);
+        heap.add(5);
+        heap.add(10);
+        heap.add(1);
+        // heap.add(0);
+
         minHeap.print();
     }
 }
