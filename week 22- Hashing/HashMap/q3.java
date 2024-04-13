347da549c4132f70b7d1e49dfb3037e458b94ec1
Ishan Gupta <ishangupta12345@yahoo.com>
2024-04-13 23:52:44 +0530

    Update q3.java

--- week 22- Hashing/HashMap/q3.java
+++ week 22- Hashing/HashMap/q3.java
@@ -1,15 +1,39 @@
 import java.util.*;
 public class q3 {
-    public static void main(String[] args) {
-        String s = "cccaaa";
-        // String s = "tree";
+    static class info implements Comparable<info> {
+        Character c;
+        int freq;
 
-        HashMap <String,Integer> map = new HashMap<>();
+        info(char c,int f){
+            this.c=c;
+            this.freq=f;
+        }        
+
+        @Override
+        public int compareTo(info i2){
+            return i2.freq - this.freq;
+        }
+    }
+    public static void main(String[] args) {
+        // String s = "cccaaa";
+        String s = "tree";
+
+        HashMap <Character,Integer> map = new HashMap<>();
 
         for (int i=0; i<s.length();i++) {
-            if (map.containsKey(map.)) {
-                
-            }            
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);        
+        }
+
+        PriorityQueue <info> pq = new PriorityQueue<>();
+        for (Character ch : map.keySet()) {
+            pq.add(new info(ch,map.get(ch)));
+        }
+
+        while (!pq.isEmpty()) {
+            for (int i =0; i<pq.peek().freq; i++) {
+                System.out.print(pq.peek().c);
+            }
+            pq.remove();
         }
     }
 }
